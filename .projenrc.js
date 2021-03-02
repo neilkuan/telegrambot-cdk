0d250753658e2fa9c041d69671e8190f5e4bbda1
Neil Guan <guan840912@gmail.com>
2021-03-02 21:31:30 +0800

    remove token

--- .projenrc.js
+++ .projenrc.js
@@ -14,6 +14,6 @@ const project = new AwsCdkTypeScriptApp({
   ],
   dependabot: false,
 });
-const ignore = ['config.ini', 'dependabot.yml', 'venv'];
+const ignore = ['config.ini', 'dependabot.yml', 'venv', 'main.test.ts.snap'];
 project.gitignore.exclude(...ignore);
 project.synth();
